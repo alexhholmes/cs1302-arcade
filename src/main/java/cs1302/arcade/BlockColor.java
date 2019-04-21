db39e4cb03236cd2746e7615029b8aea1336cf62
Alex Holmes <ahh55636@uga.edu>
2019-04-20 23:28:18 -0400

    Add BlockColor enum class

---
+++ src/main/java/cs1302/arcade/BlockColor.java
@@ -0,0 +1,23 @@
+package cs1302.arcade;
+
+/**
+ * Enum representing the colors of the Tetris game blocks.
+ */
+public enum BlockColor {
+    /** SquareBlock */
+    YELLOW,
+    /** RightLBlock */
+    ORANGE,
+    /** LeftLBlock */
+    DARK_BLUE,
+    /** LineBlock */
+    LIGHT_BLUE,
+    /** RightZBlock */
+    GREEN,
+    /** LEFTZBlock */ 
+    RED,
+    /** TBlock */
+    PURPLE,
+    /** Background */
+    BLACK
+} // BlockColor
