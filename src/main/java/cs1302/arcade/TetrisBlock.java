e9ce75001d324fe84dff53b60f9f29b6ec57ae0e
Alex Holmes <ahh55636@uga.edu>
2019-04-20 23:28:56 -0400

    Remove enum from TetrisBlock

--- src/main/java/cs1302/arcade/TetrisBlock.java
+++ src/main/java/cs1302/arcade/TetrisBlock.java
@@ -1,24 +1,6 @@
 package cs1302.arcade;
 
-public Interface TetrisBlock{
-    public enum Color {
-        /** SquareBlock */
-        YELLOW,
-        /** RightLBlock */
-        ORANGE,
-        /** LeftLBlock */
-        DARK_BLUE,
-        /** LineBlock */
-        LIGHT_BLUE,
-        /** RightZBlock */
-        GREEN,
-        /** LEFTZBlock */ 
-        RED,
-        /** TBlock */
-        PURPLE,
-        /** Background */
-        BLACK
-    }// Color
+public Interface TetrisBlock {
     /** 
     * fetches the color of the given block
     * @return color of the given block
