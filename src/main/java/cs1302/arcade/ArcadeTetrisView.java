c0fc65e6045232e40504ec9e40af58bfe4551ad7
Alex Holmes <ahh55636@uga.edu>
2019-04-20 23:29:18 -0400

    Edit ArcadeTetrisView

--- src/main/java/cs1302/arcade/ArcadeTetrisView.java
+++ src/main/java/cs1302/arcade/ArcadeTetrisView.java
@@ -1,12 +1,15 @@
 package cs1302.arcade;
 
 import javafx.scene.Parent;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.MenuBar;
 
 public class ArcadeTetrisView {
     
     private ArcadeApp app;
-    private GridPane mainPane;
+    private VBox mainPane;
+    private MenuBar menuBar;
+    private GridPane gamePane;
 
     /**
      * Constructs a new Tetris game.
@@ -15,8 +18,23 @@ public class ArcadeTetrisView {
      */
     public ArcadeTetrisView(ArcadeApp app) {
         this.app = app;
-        mainPane = new GridPane();
+        mainPane = new VBox();
+
+        buildMainPane();
     } // ArcadeTetrisView(ArcadeApp)
+
+    /**
+     * Builds the attributes and components of the main pane for this Tetris
+     * game view.
+     */
+    private void buildMainPane() {
+        menuBar = new TetrisMenuBar();
+        gamePane = new TetrisGamePane();
+        
+        
+        mainPane.getChildren()
+            .addAll(menuBar, gamePane);
+    } // buildMainPane()
 
     /**
      * Return the parent node of this view.
