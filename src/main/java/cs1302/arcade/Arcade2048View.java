94eceb5f9c02345a87f4adae0e04dcd70fb0e1ab
Alex Holmes <ahh55636@uga.edu>
2019-04-21 22:12:05 -0400

    Add UI items to 2048 game

--- src/main/java/cs1302/arcade/Arcade2048View.java
+++ src/main/java/cs1302/arcade/Arcade2048View.java
@@ -1,5 +1,13 @@
 package cs1302.arcade;
 
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Label;
+import javafx.geometry.Pos;
+
 public class Arcade2048View {
     
     private ArcadeApp app;
@@ -28,30 +36,56 @@ public class Arcade2048View {
         Menu gameMenu = new Menu("Game");
         Menu helpMenu = new Menu("Help");
         MenuItem resetItem = new MenuItem("Reset Game");
+        MenuItem highScoresItem = new MenuItem("High Scores");
         MenuItem exitItem = new MenuItem("Exit Game");
         MenuItem helpItem = new MenuItem("How to Play");
         gameMenu.getItems()
-            .addAll(resetItem, exitItem);
+            .addAll(resetItem, highScoresItem, exitItem);
         helpMenu.getItems()
             .addAll(helpItem);
         menuBar.getChildren()
             .addAll(gameMenu, helpMenu);
+        EventHandler<ActionEvent> resetHandler = event -> {
+            controller.reset();
+        };
+        resetItem.setOnAction(resetHandler);
+        EventHandler<ActionEvent> highScoreHandler = event -> {
+            openHighScoresWindow(); // TODO
+        };
+        EventHandler<ActionEvent> exitHandler = event -> {
+            app.setSelectGameScene();
+        };
+        exitItem.setOnAction(exitHandler);
+        EventHandler<ActionEvent> helpHandler = event -> {
+            openHelpWindow();
+        };
         // Game label
         Label gameLabel = new Label("2048");
         // Score
-        VBox scoreVBox = new VBox();
+        VBox scoreVBox = new VBox(4);
         Label scoreTitle = new Label("Score:");
         Label scoreLabel = new Label();
-        scoreVBox
+        scoreLabel.textProperty().bind(model.scoreProperty());
+        scoreVBox.getChildren()
+            .addAll(scoreTitle, scoreLabel);
+        scoreVbox.setAlignment(Pos.CENTER);
         // Board
-        board = new BoardGroup();
+        board = new BoardGroup(); // TODO
         // GridPane
         gridPane = new GridPane();
-        gridPane.getChildren()
-            .addAll(gameLabel, scoreVBox); // TODO
+        gridPane.add(gameLabel, 0, 0, 1, 1);
+        gridPane.add(scoreVBox, 1, 0, 1, 1);
+        gridPane.add(board, 0, 1, 2, 1);
+        gridPane.setHAlignment(gameLabel, HPos.Center);
+        gridPane.setVAlignment(gameLabel, VPos.Center);
+        gridPane.setHAlignment(scoreVBox, HPos.Center);
+        gridPane.setVAlignment(scoreVBox, VPos.Center);
+        GridPane.setHGrow(gameLabel, Priority.ALWAYS);
+        GridPane.setHGrow(scoreVBox, Priority.ALWAYS);
         gridPane.setHgap(10);
         gridPane.setVgap(10);
         // Root
+        root = new VBox();
         root.getChildren()
             .addAll(menuBar, gridPane);
     } // buildView()
