60fadebf93409e4874af577b64142457ff8281d6
Alex Holmes <ahh55636@uga.edu>
2019-05-04 14:42:32 -0400

    Add direction enum

---
+++ src/main/java/cs1302/arcade/Direction.java
@@ -0,0 +1,15 @@
+package cs1302.arcade;
+
+/**
+ * A direction of movement for a game.
+ */
+public enum Direction {
+    /** Move up */
+    UP,
+    /** Move Down */
+    DOWN,
+    /** Move Left */
+    LEFT,
+    /** Move Right */
+    RIGHT
+} // Direction
