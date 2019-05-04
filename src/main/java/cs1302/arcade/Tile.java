380680f0bed671429dc936e2df4ececf5c9df635
Alex Holmes <ahh55636@uga.edu>
2019-05-03 21:41:22 -0400

    Fix 2048 tile

--- src/main/java/cs1302/arcade/Tile.java
+++ src/main/java/cs1302/arcade/Tile.java
@@ -9,13 +9,24 @@ import javafx.beans.property.SimpleIntegerProperty;
  */
 public class Tile {
 
+    /** Value of this tile */
     private IntegerProperty value = new SimpleIntegerProperty();
+    /** Color of this tile */
+    private Color color;
+
+    /**
+     * Constructs a new tile with a value of zero.
+     */
+    public Tile() {
+        resetTile();
+    } // Tile()
     
     /**
      * Constructs a new tile with the specified value.
      */
     public Tile(int value) {
         this.value.setValue(value);
+        setColor(value);
     } // Tile(int)
 
     /**
@@ -48,10 +59,20 @@ public class Tile {
     } // mergeTile(Tile)
 
     /**
+     * Returns the color of this tile.
+     *
+     * @return the color of this tile
+     */
+    public Color getColor() {
+        return color;
+    } // getColor()
+
+    /**
      * Resets the value of this tile to zero.
      */
     public void resetTile() {
         score.setValue(0);
+        setColor(0);
     } // resetTile()
 
     /**
@@ -62,5 +83,54 @@ public class Tile {
     public boolean isEmpty() {
         return getValue() == 0 ? true : false;
     } // isEmpty()
+
+    /**
+     * Sets the color of this tile based on the paramater value.
+     *
+     * @param value the value of this tile
+     */
+    private void setColor(int value) {
+        // TODO fix colors
+        switch (value) {
+            case 0:
+                color = Color.web("#CCC0B3");
+            case 2:
+                color = Color.web("#EEE4DA");
+                break;
+            case 4:
+                color = Color.web("#EEE4DA");
+                break;
+            case 8:
+                color = Color.web("#F2B179");
+                break;
+            case 16:
+                color = Color.web("#F2B179");
+                break;
+            case 32:
+                color = Color.web("#F2B179");
+                break;
+            case 64:
+                color = Color.web("#F2B179");
+                break;
+            case 128:
+                color = Color.web("#EDCF72");
+                break;
+            case 256:
+                color = Color.web("#EDCF72");
+                break;
+            case 512:
+                color = Color.web("#EDCF72");
+                break;
+            case 1024:
+                color = Color.web("#EDCF72");
+                break;
+            case 2048:
+                color = Color.web("#EDCF72");
+                break;
+            case 4096:
+                color = Color.web("#3E3933");
+                break;
+        } // switch
+    } // setColor(int)
 
 } // Tile
