4454b3455663be71792640aa3cbacc5c9456b511
Alex Holmes <ahh55636@uga.edu>
2019-05-05 17:07:52 -0400

    Add controls and view elements

--- src/main/java/cs1302/arcade/Arcade2048View.java
+++ src/main/java/cs1302/arcade/Arcade2048View.java
@@ -13,13 +13,22 @@ import javafx.scene.shape.Rectangle;
  * The UI elements of a 2048 game.
  */
 public class Arcade2048View {
+
+    private static final double TILE_HEIGHT = 100;
+    private static final double TILE_WIDTH = 100;
     
     private ArcadeApp app;
     private VBox root;
     private GridPane gridPane;
-    private BoardGroup board;
-    private Model2048 model;
+    private MenuBar menuBar;
+    private VBox scoreVBox;
+    private Group board;
+
+    private Score score;
+    private Model2048 boardModel;
     private Controller2048 controller;
+
+    private boolean keyDisable = false;
     
     /**
      * Starts a new game of 2048.
@@ -27,58 +36,26 @@ public class Arcade2048View {
     public Arcade2048View(ArcadeApp app) {
         // Instance variables
         this.app = app;
-        model = new Model2048();
-        controller = new Controller2048(model);
+        score = new Score();
+        boardModel = new Model2048();
+        controller = new Controller2048(boardModel, score);
         
         // Build UI
         buildView();
-
-        // Start game
-        controller.startGame();
+        // Game controls
+        board.setKeyPressed(createKeyHandler());
+        board.requestFocus();
     } // Arcade2048View(ArcadeApp)
 
     /**
      * Builds the game view.
      */
     public void buildView() {
-        // MenuBar
-        MenuBar menuBar = new MenuBar();
-        Menu gameMenu = new Menu("Game");
-        Menu helpMenu = new Menu("Help");
-        MenuItem resetItem = new MenuItem("Reset Game");
-        MenuItem exitItem = new MenuItem("Exit Game");
-        MenuItem helpItem = new MenuItem("How to Play");
-        gameMenu.getItems()
-            .addAll(resetItem, highScoresItem, exitItem);
-        helpMenu.getItems()
-            .addAll(helpItem);
-        menuBar.getChildren()
-            .addAll(gameMenu, helpMenu);
-        // MenuItem event handlers
-        EventHandler<ActionEvent> resetHandler = event -> {
-            controller.reset();
-        };
-        resetItem.setOnAction(resetHandler);
-        EventHandler<ActionEvent> exitHandler = event -> {
-            app.setSelectGameScene();
-        };
-        exitItem.setOnAction(exitHandler);
-        EventHandler<ActionEvent> helpHandler = event -> {
-            openHelpWindow();
-        };
-        helpItem.setOnAction(helpHandler);
-        // Game label
-        Label gameLabel = new Label("2048");
-        // Score
-        VBox scoreVBox = new VBox(4);
-        Label scoreTitle = new Label("Score:");
-        Label scoreLabel = new Label();
-        scoreLabel.textProperty().bind(model.scoreProperty());
-        scoreVBox.getChildren()
-            .addAll(scoreTitle, scoreLabel);
-        scoreVbox.setAlignment(Pos.CENTER);
-        // Board
-        board = new BoardGroup(); // TODO
+        // Components
+        buildMenuBar();
+        buildGameLabel();
+        buildScore();
+        buildBoard();
         // GridPane
         gridPane = new GridPane();
         gridPane.add(gameLabel, 0, 0, 1, 1);
@@ -99,55 +76,123 @@ public class Arcade2048View {
     } // buildView()
 
     /**
-     * Binds the board UI tiles to the board model tiles.
+     * Builds the menu bar.
      */
-    private void bindBoard() {
-        Tile[][] board = model.getBoard();
-        for (int row = 0; row < board.length; row++) {
-            for (int col = 0; col < board[row].length; col++) {
-                Tile tile = board[row][col];
-                IntegerProperty value = tile.valueProperty();
-                Color color = tile.getColor();
-                bindTile(row, col, value, color);
+    private void buildMenuBar() {
+        menuBar = new MenuBar();
+        Menu gameMenu = new Menu("Game");
+        Menu helpMenu = new Menu("Help");
+        MenuItem resetItem = new MenuItem("Reset Game");
+        MenuItem exitItem = new MenuItem("Exit Game");
+        MenuItem helpItem = new MenuItem("How to Play");
+        gameMenu.getItems()
+            .addAll(resetItem, highScoresItem, exitItem);
+        helpMenu.getItems()
+            .addAll(helpItem);
+        menuBar.getChildren()
+            .addAll(gameMenu, helpMenu);
+        // MenuItem event handlers
+        EventHandler<ActionEvent> resetHandler = event -> {
+            controller.reset();
+        };
+        EventHandler<ActionEvent> exitHandler = event -> {
+            app.setSelectGameScene();
+        };
+        EventHandler<ActionEvent> helpHandler = event -> {
+            displayHelpWindow();
+        };
+        resetItem.setOnAction(resetHandler);
+        exitItem.setOnAction(exitHandler);
+        helpItem.setOnAction(helpHandler);
+    } // buildMenuBar()
+
+    /**
+     * Builds the 2048 game label.
+     */
+    private void buildGameLabel() {
+        Label gameLabel = new Label("2048");
+    } // buildGameLabel()
+
+    /**
+     * Builds the score display.
+     */
+    private void buildScore() {
+        scoreVBox = new VBox(4);
+        Label scoreTitle = new Label("Score:");
+        Label scoreLabel = new Label();
+        scoreLabel.textProperty().bind(model.scoreProperty());
+        scoreVBox.getChildren()
+            .addAll(scoreTitle, scoreLabel);
+        scoreVbox.setAlignment(Pos.CENTER);
+    } // buildScore()
+
+    /**
+     * Builds the game board.
+     */
+    private void buildBoard() {
+        board = new Group();
+        board.minHeight(450);
+        board.minWidth(450);
+        for (int row = 0; row < board.getRows(); row++) {
+            for (int col = 0; col < board.getCols(); col++) {
+                Tile tile = board.getTile(row, col);
+                TileView tileView = new TileView();
+                tileView.xProperty().bind(tile.xProperty);
+                tileView.yProperty().bind(tile.yProperty);
+                tileView.textProperty().bind(tile.valueProperty().asString());
+                tile.valueProperty().addListener((obs, oldValue, newValue) ->
+                    (tileView.setFill(tile.getColor()));
+                board.getChildren.add(tile);
             } // for
         } // for
-    } // bindBoard()
+    } // buildBoard()
+
+    private EventHandler<? super KeyEvent> createKeyHandler() {
+        return event -> {
+            System.out.println(event.getCode());
+            if (!keyDisable) {
+                keyDisable = true;
+                } // switch
+                Thread t = new Thread(() -> {
+                    switch (event.getCode()) {
+                        case KeyCode.UP:
+                            controller.makeMove(Direction.UP);
+                            break;
+                        case KeyCode.DOWN:
+                            controller.makeMove(Direction.DOWN);
+                            break;
+                        case KeyCode.LEFT:
+                            controller.makeMove(Direction.LEFT);
+                            break;
+                        case KeyCode.RIGHT:
+                            controller.makeMove(Direction.RIGHT);
+                            break;
+                    makeMove(direction);
+                });
+                thread.setDaemon(true);
+                thread.start();
+                if (controller.isGameOver()) {
+                    displayGameOver();
+                } else {
+                    keyDisable = false;
+                } // if
+            } // if
+        };
+    } // createKeyHandler()
 
     /**
-     * Binds a tile to its corresponding board data model value and sets its
-     * color.
-     *
-     * @param row the row of the tile
-     * @param col the column of the tile
-     * @param value the value of the tile piece
-     * @param color the color of the tile piece
+     * Displays a game over screen when the game is complete.
      */
-    private void bindTile(int row, int col, IntegerProperty value, Color color) {
-        StackPane tile = board.getTile(row, col);
-        // Bind UI tile text to display the tile integer value
-        tile.get(1).textProperty().bind(value.asString());
-        setTileColor(tile.getChildren(0), color);
-    } // bindTile(int, int, IntegerProperty, Color)
-
-    /**
-     * Sets the CSS for the tile to the specified color. A color of black indicates
-     * that the board space is empty.
-     *
-     * @param tile a rectangle representing a game tile
-     * @param color the color of the tile piece
-     */
-    private void setTileColor(Rectangle tile, Color color) {
-        // TODO
-        switch (color) {
-            case BLACK:
-                tile.applyCSS(); // TODO
-                break;
-        }
-    } // setTileColor(Rectangle, Color)
-
-    private void tileColorListener() {
+    private void displayGameOver() {
         
-    } // tileColorListener
+    } // displayGameOver()
+
+    /**
+     * Displays the rules of the game and its controls.
+     */
+    private void displayHelpWindow() {
+
+    } // displayHelpWindow()
 
     /**
      * Returns the root node of this game as a {@code Parent}.
