59d3b811363e2e607a98e9a7b0395c1b7c0a3d3e
KEVIN WALSH <walsh@nike.cs.uga.edu>
2019-05-05 18:19:34 -0400

    Update gameflow of tetris

--- src/main/java/cs1302/arcade/ControllerTetris.java
+++ src/main/java/cs1302/arcade/ControllerTetris.java
@@ -5,16 +5,37 @@ import java.util.Random;
 public class ControllerTetris {
     public void start(){
         TetrisBoard gameBoard = new TetrisBoard();
-            TetrisPiece currentPiece = new TetrisPiece(Random.nextInt(7)+1);
-                Timeline tLine = new Timeline();
-                EventHandler<ActionEvent> handler = event ->{
-                    if(gameBoard.canDrop(currentPiece){
-                        gameBoard.dropPiece(currentPiece);
-                    }// if
-                };
-                KeyFrame frame = new KeyFrame(Duration.seconds(2), handler);
-                tLine.setCycleCount(Timeline.INDEFINITE);
-                tLine.getKeyFrames().add(frame);
-                tLine.play();
+        TetrisPiece currentPiece = new TetrisPiece(Random.nextInt(7)+1);
+        TetrisPiece nextPiece = new TetrisPiece(Random.nextInt(7)+1);
+        int score = 0;
+        gameBoard.addPiece(currentPiece);
+        boolean falling = true;
+        
+        Timeline tLine = new Timeline();
+        EventHandler<ActionEvent> handler = event ->{
+            if(falling){
+                gameBoard.dropPiece(currentPiece);
+            }// if
+        };
+        KeyFrame frame = new KeyFrame(Duration.seconds(2), handler);
+        tLine.setCycleCount(Timeline.INDEFINITE);
+        tLine.getKeyFrames().add(frame);
+        tLine.play();
+        
+        while(1 == 1){
+            if(gameBoard.canDrop(currentPiece) == false){
+                score += gameBoard.calcScore();
+                currentPiece = nextPiece;
+                nextPiece = new TetrisPiece(Random.nextInt(7)+1);
+                if(gameBoard.canAddToTop(currentPiece)){
+                    gameBoard.addPiece(currentPiece);
+                }// if
+                else{
+                    falling = false;
+                    gameBoard.addPiece(currentPiece);
+                    
+                }// else
+            }// if
+        }// while
     }// 
 }// ControllerTetris
